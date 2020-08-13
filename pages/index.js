5fb55811e9226109ad1b0c5108af3902b2b1c67d
Justin Geiger <jgeiger@rightpoint.com>
2020-08-13 09:49:08 -0500

    initial

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <h3>Environment Variable</h3>
+            <p>
+            EXT_SERVER: {process.env.EXT_SERVER} <br />
+            SERVER_LOCAL: {process.env.SERVER_LOCAL} <br />
+            NEXT_PUBLIC_EXT_BROWSER: {process.env.NEXT_PUBLIC_EXT_BROWSER} <br />
+            NEXT_PUBLIC_BROWSER_LOCAL: {process.env.NEXT_PUBLIC_BROWSER_LOCAL} <br />
+            </p>
+          </div>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
